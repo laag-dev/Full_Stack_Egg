2218a15e8b835e51b7e3eea5fb00ac7f6189d4fa
laag-dev <laag.08@gmail.com>
2023-05-15 01:14:15 -0300

    actualizando mi repositorio de ejercicios de poo

---
+++ JavaPoo/src/javaPoo/Ejercicio8.java
@@ -0,0 +1,28 @@
+package javaPoo;
+
+import Entidad.Cadena;
+import Service.CadenaService;
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+//  Realizar una clase llamada Cadena que tenga como atributos una frase (de tipo de
+//String) y su longitud. En el main se creará el objeto y se le pedirá al usuario que ingrese
+//una frase que puede ser una palabra o varias palabras separadas por un espacio en
+//blanco y a través de los métodos set, se guardará la frase y la longitud de manera
+//automática según la longitud de la frase ingresada. Deberá además implementar los métodos
+        CadenaService cadena = new CadenaService();
+        
+        Cadena cad = cadena.crearPalabra();
+        cadena.mostarVocales(cad);
+        cadena.invertirFrase(cad);
+        cadena.vecesRepetido(cad);
+        cadena.compararLongitud(cad);
+        cadena.unirFrases(cad);
+        cadena.reemplazar(cad);
+        cadena.contiene(cad);
+        
+    }
+}
