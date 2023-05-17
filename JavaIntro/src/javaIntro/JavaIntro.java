d7f8a6db17313d88b7367dec3adbef4cf0b33539
laag-dev <laag.08@gmail.com>
2023-05-17 01:27:39 -0300

    actualizando mi repositorio de ejercicios de JavaIntro

--- JavaIntro/src/javaIntro/JavaIntro.java
+++ JavaIntro/src/javaIntro/JavaIntro.java
@@ -1,29 +1,16 @@
-// <editor-fold defaultstate="collapsed" desc=" Ejercicio 21 ">
-
-
-
-
-// </editor-fold>
-
-package EjeciciosJava;
+package javaIntro;
 
 import java.util.Scanner;
 
+public class JavaIntro {
 
+    static Scanner leer = new Scanner(System.in); //variable global
 
-public class EjeciciosJava {
-
-  static Scanner leer = new Scanner(System.in); //variable global
-    
     public static void main(String[] args) {
-       
-    
-// <editor-fold defaultstate="collapsed" desc=" Ejercicio 1 ">
 
+// <editor-fold defaultstate="collapsed" desc=" Ejercicio 1 ">
 //Escribir un programa que pida dos números enteros por teclado y calcule la suma de los
 ////dos. El programa deberá después mostrar el resultado de la suma
-
-
 //  
 //        System.out.println("Ingrese un numero");
 //        int num1 = leer.nextInt();
@@ -35,71 +22,50 @@ public class EjeciciosJava {
 //        int suma = num1 +num2;
 //        
 //        System.out.println("El resultados de la suma de ambos numeros es: " + suma);
-
 // </editor-fold> 
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 2 ">
-
 //Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por
 //pantalla.
-
 //      Scanner leer = new Scanner(System.in);
 //         
 //          System.out.println("Por favor ingrese su nombre");
 //        String nombre = leer.nextLine();
 //        
 //        System.out.println("Su nombre es: " + nombre);
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 3 ">
-
 //Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda
 //en minúsculas. Nota: investigar la función toUpperCase() y toLowerCase() en
 //Java.
-
 //       String frase;
 //       System.out.println("Ingrese frase: ");
 //       frase = leer.nextLine();
 //       
 //       System.out.println("La frase en mayus es:"+frase.toUpperCase());
 //       System.out.println("La frase en minus es: "+frase.toLowerCase());
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 4 ">
-
 //Dada una cantidad de grados centígrados se debe mostrar su equivalente en grados
 //Fahrenheit. La fórmula correspondiente es: F = 32 + (9 * C / 5).
-
 //        float f, c;
 //        System.out.print("Ingrese el valor en centigrados: ");
 //        c = leer.nextFloat();
 //        f = (32+(9*c/5));
 //        System.out.println("La temperatura den Fahrenheit es: " + f);
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 5 ">
-
 //Escribir un programa que lea un número entero por teclado y muestre por pantalla el
 //doble, el triple y la raíz cuadrada de ese número. Nota: investigar la función
 //Math.sqrt().
-
 //    int num;
 //        System.out.println("Ingrese un numero: ");
 //        num= read.nextInt();
 //        System.out.println("El doble del numero es: "+ (num*2));
 //        System.out.println("El triple del numero es: "+ (num*3));
 //        System.out.println("La raiz cuadrada del numero es: "+Math.sqrt(num));
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 6 ">
-
 //Crear un programa que dado un numero determine si es par o impar.
-
 //int num;
 //          System.out.println("Ingrese un numero");
 //          num= leer.nextInt();
@@ -109,15 +75,11 @@ public class EjeciciosJava {
 //          else {
 //              System.out.println("El numero es impar");
 //          }
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 7 ">
-
 //Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa
 //pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. Nota:
 //investigar la función equals() en Java.
-
 //System.out.println("Ingrese contraseña");
 //String frase = leer.nextLine();
 //frase = toLowerCase(frase);
@@ -128,16 +90,12 @@ public class EjeciciosJava {
 //else {
 //System.out.println("Su contraseña es incorrecta");
 //}
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 8 ">
-
 //Realizar un programa que solo permita introducir solo frases o palabras de 8 de largo. Si
 //el usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje
 //por pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir
 //“INCORRECTO”. Nota: investigar la función Lenght() en Java.
-
 //System.out.println("Ingrese frase");
 //String frase = leer.nextLine();
 //int longitud = frase.length();
@@ -147,17 +105,12 @@ public class EjeciciosJava {
 //else {
 //    System.out.println("Incorrecto no tiene 8 caracteres");
 //}
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 9 ">
-
 //Escriba un programa que pida una frase o palabra y valide si la primera letra de esa frase
 //es una ‘A’. Si la primera letra es una ‘A’, se deberá de imprimir un mensaje por pantalla
 //que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. Nota:
 //investigar la función Substring y equals() de Java.
-
 //System.out.println("Ingrese una frase");
 //        String frase=leer.nextLine();
 //        
@@ -167,16 +120,11 @@ public class EjeciciosJava {
 //        }else{
 //            System.out.println("Incorrecto");
 //        }
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 10 ">
-
 //Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación
 //solicite números al usuario hasta que la suma de los números introducidos supere el
 ////límite inicial.
-
 //int suma=0;
 //        
 //        System.out.println("Ingrese el valor numero limite positivo");
@@ -191,10 +139,7 @@ public class EjeciciosJava {
 //        } while (suma<=limite);
 ////        
 //        System.out.println("La suma es: " +suma);
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 11 ">
 //  
 ////Realizar un programa que pida dos números enteros positivos por teclado y muestre por
@@ -206,14 +151,12 @@ public class EjeciciosJava {
 //4. Dividir
 //5. Salir
 //Elija opción:
-
 //El usuario deberá elegir una opción y el programa deberá mostrar el resultado por
 //pantalla y luego volver al menú. El programa deberá ejecutarse hasta que se elija la
 //opción 5. Tener en cuenta que, si el usuario selecciona la opción 5, en vez de salir del
 //programa directamente, se debe mostrar el siguiente mensaje de confirmación: ¿Está
 //seguro que desea salir del programa (S/N)? Si el usuario selecciona el carácter ‘S’ se sale
 ////del programa, caso contrario se vuelve a mostrar el menú.
-
 //int opc=0;
 //    String salir;      
 //        
@@ -304,11 +247,8 @@ public class EjeciciosJava {
 //    System.out.println("Ingrese el segundo numero");
 //    int num2 = leer.nextInt();
 //    System.out.println("El resultado es: "+(num1/num2));
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 12 ">
-
 //Realizar un programa que simule el funcionamiento de un dispositivo RS232, este tipo de
 //dispositivo lee cadenas enviadas por el usuario. Las cadenas deben llegar con un formato
 //fijo: tienen que ser de un máximo de 5 caracteres de largo, el primer carácter tiene que
@@ -320,7 +260,6 @@ public class EjeciciosJava {
 //e incorrectas recibidas. Para resolver el ejercicio deberá investigar cómo se utilizan las
 //siguientes funciones de Java Substring(), Length(), equals().
 //
-
 //        System.out.println("Ingrese una cadena con 5 caracteres como maximo,");
 //        System.out.println("El primer caracter inicia en X y el último en O");
 //        String cadena;
@@ -343,9 +282,7 @@ public class EjeciciosJava {
 //                }
 //        }while(!cadena.equals("&&&&&"));
 //        System.out.println("La cantidad de frase ingresadas es: "+(i-1)+" con "+contador+" correctas y "+(i-1-contador)+" incorrectas.");
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 13 ">
 //Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. Por ejemplo, si el
 //cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
@@ -356,7 +293,6 @@ public class EjeciciosJava {
 //
 //* *
 //* * * *
-
 //System.out.println("Ingrese N elementos por lado: ");
 //        int n = leer.nextInt();
 //        
@@ -373,9 +309,7 @@ public class EjeciciosJava {
 //        } 
 //        
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 14 ">
-
 //Crea una aplicación que a través de una función nos convierta una cantidad de euros
 //introducida por teclado a otra moneda, estas pueden ser a dólares, yenes o libras. La
 //función tendrá como parámetros, la cantidad de euros y la moneda a converir que será
@@ -385,7 +319,6 @@ public class EjeciciosJava {
 //* 0.86 libras es un 1 €
 //* 1.28611 $ es un 1 €
 //* 129.852 yenes es un 1 €
-
 //System.out.println("Ingrese la cantidad de euros a transformar ");
 //
 //        int euro=leer.nextInt();
@@ -428,14 +361,10 @@ public class EjeciciosJava {
 //        }
 //
 //        System.out.println("La conversion es de euro a " + op + " es de " + Cd);     
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 15 ">
-
 //Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
 //muestre por pantalla en orden descendente.
-
 //   int []vector = new int [100];
 //   
 //   for (int i=0;i<=99;i++) {
@@ -449,15 +378,11 @@ public class EjeciciosJava {
 //        
 //   }
 // 
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 16 ">
-
 //Realizar un algoritmo que rellene un vector de tamaño N con valores aleatorios y le pida
 //al usuario un numero a buscar en el vector. El programa mostrará donde se encuentra el
 //numero y si se encuentra repetido
-
 //    int []vector = new int [80];
 //      
 //        int numEncontrar, contador=0;     
@@ -493,16 +418,9 @@ public class EjeciciosJava {
 //        vector [i] = (int) (Math.random()*100);
 //    }
 //     return vector;   
-       
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 17 ">
-
-
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 18 ">
 //
 //    int matriz [][] = new int [4][4];
@@ -534,16 +452,13 @@ public class EjeciciosJava {
 ////            }  
 ////         System.out.println("");
 //        }              
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 19 ">
 //
 //Realice un programa que compruebe si una matriz dada es anti simétrica. Se dice que
 //una matriz A es anti simétrica cuando ésta es igual a su propia traspuesta, pero cambiada
 //de signo. Es decir, A es anti simétrica si A = -AT. La matriz traspuesta de una matriz A se
 //denota por AT y se obtiene cambiando sus filas por columnas (o viceversa).
-
 ////Scanner read = new Scanner(System.in);
 //        int num;
 //        boolean anti;
@@ -577,11 +492,8 @@ public class EjeciciosJava {
 //                }
 //                } 
 //        }
-        
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 20 ">
-
 //Un cuadrado mágico 3 x 3 es una matriz 3 x 3 formada por números del 1 al 9 donde la
 //suma de sus filas, sus columnas y sus diagonales son idénticas. Crear un programa que
 //permita introducir un cuadrado por teclado y determine si este cuadrado es mágico o no.
@@ -600,8 +512,6 @@ public class EjeciciosJava {
 //                    matriz[i][j] = leer.nextInt();
 //            }
 //        }
-
-
 //       
 //Scanner read = new Scanner(System.in);
 //        int matrix[][] = new int[3][3];
@@ -665,11 +575,7 @@ public class EjeciciosJava {
 //        }
 //    }
 //}
-
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 1 extra">
 //
 //System.out.println("Ingrese los minutos para pasar en dias y horas: ");
@@ -684,11 +590,8 @@ public class EjeciciosJava {
 //            horas = horas%24;
 //            
 //            System.out.println(minutos + " minutos equivalen a " + dias + " días y " + horas + " horas.");
-        
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 2 extra ">
-
 //int A = 1, B = 2, C = 3, D = 4;
 //    int aux;
 //    
@@ -709,12 +612,8 @@ public class EjeciciosJava {
 //         System.out.println("B: "+B);
 //          System.out.println("C: "+C);
 //           System.out.println("D: "+D);
-
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 3 extra ">
-
 //    System.out.println("Ingrese una letra: ");
 //        String letra = leer.next().toLowerCase();
 //        
@@ -723,12 +622,8 @@ public class EjeciciosJava {
 //        }else{
 //            System.out.println("La letra no es una vocal");
 //        }
-
 // </editor-fold>
-
 // <editor-fold defaultstate="collapsed" desc=" Ejercicio 4 extra">
-
-
 //System.out.println("Ingrese un numero entre 1 al 10 apra pasarlo en romano: ");
 //        int numero = leer.nextInt();
 //        
@@ -739,10 +634,7 @@ public class EjeciciosJava {
 //            
 //            System.out.println("El número "+numero+" en números romanos es: "+romanos[numero-1]);
 //        }
-
 // </editor-fold>
+    }//fin main
 
-
-}//fin main
-       
 }// fin class
