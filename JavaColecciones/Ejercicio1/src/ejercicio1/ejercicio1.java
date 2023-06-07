ba2fd8694fad9ddf76527386357f218a401e19a3
laag-dev <laag.08@gmail.com>
2023-06-07 00:39:44 -0300

    actualizando mi repositorio de ejercicios de colecciones

--- JavaColecciones/Ejercicio1/src/ejercicio1/ejercicio1.java
+++ JavaColecciones/Ejercicio1/src/ejercicio1/ejercicio1.java
@@ -7,25 +7,33 @@ public class ejercicio1 {
 
     public static void main(String[] args) {
 
-        Scanner consola = new Scanner(System.in).useDelimiter("\n");
-
-        ArrayList<String> razasPerros = new ArrayList();
-        String raza;
-
-        while (true) {
-
-            System.out.println("Ingrese la raza del perro o ingrese 'Salir' para terminar de llenar la información: ");
-            raza = consola.nextLine().toLowerCase();
-            if (raza.equalsIgnoreCase("salir")) {
-                break;
-            }
-            razasPerros.add(raza);
-        }
-
-        System.out.println("Razas de perros guardadas: ");
-
-        for (String razaPerro : razasPerros) {
-            System.out.println(razaPerro);
-        }
-    }
-} //fin
+//Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo String. El
+//programa pedirá una raza de perro en un bucle, el mismo se guardará en la lista y
+//después se le preguntará al usuario si quiere guardar otro perro o si quiere salir. Si decide
+//salir, se mostrará todos los perros guardados en el ArrayList.
+        
+        
+     Scanner consola = new Scanner(System.in); 
+    
+     ArrayList<String> razaPerro = new ArrayList(); 
+     String respuestaFin;
+     int cont =1;
+     
+        do {
+            
+            System.out.println("Ingrese la raza del perro a la base de datos: ");
+            razaPerro.add(consola.nextLine().toUpperCase());
+            System.out.println("¿Desea agregar otra raza? Ingrese SI o NO");
+            respuestaFin = consola.nextLine().toUpperCase();
+            
+        } while (respuestaFin.equals("SI"));
+        
+        System.out.println("********BASE DE DATOS DE RAZAS DE PERRO********");
+        for(String aux :razaPerro){
+            
+            System.out.println(cont + " ----> " + aux);
+            cont++;
+        }  
+    
+    }  
+}//fin
--- JavaColecciones/Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ JavaColecciones/Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio3;
 
-/**
- *
- * @author laag0
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Ejercicio3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    public static void main(String[] args) {
+  
